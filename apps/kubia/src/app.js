371b087eb68cf6801d0a662d4add1a4d8fb64887
Юрий Астахов <y.astakhov@urent.city>
2023-06-24 21:35:54 +0300

    kubia v1

--- apps/kubia/src/app.js
+++ apps/kubia/src/app.js
@@ -3,18 +3,10 @@ const os = require('os');
 
 console.log('Kubia server starting...');
 
-var requestCount = 0;
-
 var handler = function(request, response) {
     console.log('Received request from ' + request.connection.remoteAddress);
-    requestCount++;
-    if (requestCount > 5) {
-        response.writeHead(500);
-        response.end("I'm not well. Please restart me!");
-        return;
-    }
     response.writeHead(200);
-    response.end("You've hit " + os.hostname() + '\n');
+    response.end("This is v1 running in pod " + os.hostname() + '\n');
 }
 
 var www = http.createServer(handler);
